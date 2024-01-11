e20cb25fcec451d6830e5381589dc7df2a534a7e
salihabrik <briksaliha@gmail.com>
2024-01-11 21:43:46 +0100

    Basic annotations - to string

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,6 +4,7 @@ This module contains the function floor.
 """
 from math import floor as fl
 
+
 def floor(n: float) -> int:
     """
     Takes a float n as argument and returns the floor of the float.
