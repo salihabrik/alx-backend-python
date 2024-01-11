263941516b572979e0a16512c10d0970b7cabcfd
salihabrik <briksaliha@gmail.com>
2024-01-11 21:49:54 +0100

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains the variables a, pi, i_understand_annotations, and school.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
